f76895ce0d6ecc6d9b6435385817d58b03f386b0
Ning An <ning.an@outlook.com>
2020-07-02 16:26:03 -0400

    Exercise1-solution

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,30 @@
 // here there be JS, yarrr ☠️
+const messageInput = document.querySelector('#user-input');
+const conversationElem = document.querySelector('#conversation-container');
+
+const handleFocus = () => {
+    messageInput.focus();
+}
+
+
+const updateConversation = (message) => {
+    const { author, text } = message;
+    const messageElem =  document.createElement('p');
+    messageElem.innerHTML = `<span>${text}</span>`;
+    messageElem.classList.add('message', author);
+    conversationElem.appendChild(messageElem);
+    if (author === 'user') messageInput.value = '';
+    handleFocus();
+    conversationElem.scrollTop = conversationElem.scrollHeight;
+}
+
+const sendMessage = (e) => {
+    e.preventDefault();
+    const message = { author: 'user', text: messageInput.value};
+    updateConversation(message);
+    fetch('/cat-message')
+        .then( res => res.json() )
+        .then( data => updateConversation(data.message) )
+}
+
+handleFocus();
